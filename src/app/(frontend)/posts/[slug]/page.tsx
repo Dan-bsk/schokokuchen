fffb593d40868219148115cd4f3669a7128d3734
Dan-bsk <bosnjak.daniel1994@gmail.com>
2025-07-03 19:30:19 +0200

    Typo

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,10 +1,11 @@
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { PortableText } from "next-sanity";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
-import { notFound } from "next/navigation";
-import Link from "next/link";
 import { urlFor } from "@/sanity/lib/image";
-import Image from "next/image";
-
+import { components } from "@/sanity/portableTextComponents";
 
 export default async function Page({
   params,
@@ -37,6 +38,11 @@ export default async function Page({
         />
       ) : null}
       <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
+      {post?.body ? (
+        <div className="prose">
+          <PortableText value={post.body} components={components} />
+        </div>
+      ) : null}
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
     </main>
